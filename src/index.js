cbedc56ffaf99512b48252bca4296127aaec0d15
yash <patilyash02121@gmail.com>
2025-09-19 22:17:47 +0530

    Included PORT of .env

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { router } from "./routes.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+// const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 
 app.use(express.json());
 
