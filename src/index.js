993041ad1150d4d0cf6978b093dc0b9b6d442f0a
yash <patilyash02121@gmail.com>
2025-09-20 21:10:20 +0530

    Resolved CORS error

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import express from "express";
 import { router } from "./routes.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 // const PORT = process.env.PORT || 3000;
 const PORT = process.env.PORT;
+
+// Enable CORS
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",              // local dev
+      "https://xmltojson-frontend.onrender.com" // deployed frontend
+    ],
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 
 app.use(express.json());
 
